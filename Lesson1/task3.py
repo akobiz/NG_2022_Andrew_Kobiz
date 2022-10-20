ef06d433270220ffe633e09caac34670f524b481
Andrew Kobiz <andrewkobiz@gmail.com>
2022-10-20 09:32:03 +0300

    Tried to create converter UNIXTIME to date without modules

--- Lesson1/task3.py
+++ Lesson1/task3.py
@@ -1,6 +1,22 @@
-import datetime
 
-userSeconds = int(input("Enter a seconds: "))
-resDate = str(datetime.datetime.fromtimestamp(userSeconds))
+userSeconds = 1666243951#int(input("Enter a seconds: "))
+daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-print("Your date:", resDate.replace(" ",":")[8:])
+years =  int(userSeconds / 31536000) + 1970 # unixtime / seconds in 1 year
+years = int((years - 1969) / 4) # found extra days (leap years)
+
+days = int(userSeconds / 86400) # unixtime / seconds in 24 hrs
+secondsCurrentDay = userSeconds -   (days*86400)
+hours = int(secondsCurrentDay/3600) # found hours considering my time zone (+3)
+minutes = (secondsCurrentDay - hours * 3600) / 60
+seconds = int((minutes * 60) - (int(minutes) * 60))
+days = (days - years) % 365 # found how much days is gone in this year
+
+for i in range(len(daysInMonth)):
+    if days < 31:
+        days += 1
+        break
+    days -= daysInMonth[i]
+
+
+print("Day",days,":", hours + 3,":" ,int(minutes),":", seconds,".")
