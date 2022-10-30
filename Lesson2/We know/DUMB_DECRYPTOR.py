c283a845631b4b6c2096c565591e7aef9ae1abcf
Andrew Kobiz <andrewkobiz@gmail.com>
2022-10-30 11:01:36 +0200

    Added script that decryptes schiper of Cezar

--- Lesson2/We know/DUMB_DECRYPTOR.py
+++ Lesson2/We know/DUMB_DECRYPTOR.py
@@ -1,1 +1,18 @@
-NOPQRSTUVWXYZABCDEFGHIJMLK # cracker to decrypt :s
+from dev_log import *
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!,.-\" "
+cracker = "NOPQRSTUVWXYZABCDEFGHIJMLK!,.-\" "
+cryptedString = input("Enter your shiphered syntance: ")
+decryptedString = []
+
+for i in range(0, len(cryptedString)):
+    for j in range(0, len(cracker)):
+        if cryptedString[i].upper() == cracker[j].upper():
+            info("IF is working...")
+            decryptedString.append(alphabet[j])
+  #      elif cryptedString[i] == "!" or cryptedString[i] == "," or cryptedString == " " or cryptedString == "," and decryptedString[:-1] !=  cryptedString[i]:
+   #         info("Elif")
+   #         decryptedString.append(cryptedString[i])
+            
+for char in decryptedString:
+    print(char, end='')
