938c82caab801e8b030fd7b6657c61ef745f5e5f
Andrew Kobiz <andrewkobiz@gmail.com>
2022-10-30 11:02:58 +0200

    Made some minor edits

--- Lesson2/We know/DUMB_DECRYPTOR.py
+++ Lesson2/We know/DUMB_DECRYPTOR.py
@@ -1,5 +1,3 @@
-from dev_log import *
-
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!,.-\" "
 cracker = "NOPQRSTUVWXYZABCDEFGHIJMLK!,.-\" "
 cryptedString = input("Enter your shiphered syntance: ")
@@ -8,11 +6,7 @@ decryptedString = []
 for i in range(0, len(cryptedString)):
     for j in range(0, len(cracker)):
         if cryptedString[i].upper() == cracker[j].upper():
-            info("IF is working...")
             decryptedString.append(alphabet[j])
-  #      elif cryptedString[i] == "!" or cryptedString[i] == "," or cryptedString == " " or cryptedString == "," and decryptedString[:-1] !=  cryptedString[i]:
-   #         info("Elif")
-   #         decryptedString.append(cryptedString[i])
-            
+    
 for char in decryptedString:
     print(char, end='')
