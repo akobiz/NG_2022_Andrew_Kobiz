5013af05fe6d06f7c1a59bea8cddbde1bf22ecbe
Andrew Kobiz <andrewkobiz@gmail.com>
2022-11-04 12:40:27 +0200

    loop replaced to print

--- Lesson2/We know/DUMB_DECRYPTOR.py
+++ Lesson2/We know/DUMB_DECRYPTOR.py
@@ -8,5 +8,4 @@ for shiphredChar in range(0, len(cryptedString)):
         if cryptedString[shiphredChar].upper() == cracker[decryptorChar].upper():
             decryptedString.append(alphabet[decryptorChar])
     
-for char in decryptedString:
-    print(char, end='')
+print(''.join(decryptedString))
